d97241c538df57beea452c0b25160b76b90d8d04
huhuaping <huhuaping01@nwsuaf.edu.cn>
2022-01-23 14:32:42 +0800

    global align

--- R/globals.R
+++ R/globals.R
@@ -3,4 +3,4 @@ utils::globalVariables(c("id", "block", "vx", "vars", "bx","type","wage","lwage"
 
 utils::globalVariables(c("lx", "lx.value", "value"))
 
-utils::globalVariables(c("part", "n"))
+utils::globalVariables(c("part", "n","dif", "n_max"))
